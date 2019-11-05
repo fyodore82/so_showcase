9341ecf2e1c0d68374ff968158023c9495179376
fyodore82 <fyodor_e@hotmail.com>
2019-11-05 16:17:24 +1000

    Build folder fommit for glitch

---
+++ build/precache-manifest.020f9083359e97e9f018f63c0443e7d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a5afaef6a6c280e5fc7c54aeb32269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7cc6d1f4c3405fff07",
+    "url": "/static/css/main.789d4f6e.chunk.css"
+  },
+  {
+    "revision": "3d584697cfc2b3e9786e",
+    "url": "/static/js/2.c982e8aa.chunk.js"
+  },
+  {
+    "revision": "7c7cc6d1f4c3405fff07",
+    "url": "/static/js/main.1f4202ac.chunk.js"
+  },
+  {
+    "revision": "7a4bb4f0a8d1e4c1982f",
+    "url": "/static/js/runtime-main.fab0f97e.js"
+  }
+]);
